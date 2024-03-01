bbc1675e15bd336513874ea5d1e51dcfb1462e9e
Frankie <847145851@qq.com>
2024-03-02 02:56:56 +0800

    push library
    push library

--- EasyUi/build.gradle.kts
+++ EasyUi/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
-    id("maven-publish") //新添加的
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -27,14 +28,29 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
     implementation(project(":EasyTools"))
-
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
+}
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.github.frankie9527"
+                artifactId = "ui"
+                version = "0.5.1"
+                from(components["release"])
+            }
+        }
+    }
 }
