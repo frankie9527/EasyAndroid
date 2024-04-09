3b13eb478bf2619f3ccfe2b9424a15a6c9629f99
frankie9527 <847145851@qq.com>
2024-04-09 21:42:22 +0800

    修改logutils

--- EasyTools/src/main/java/org/easy/tools/utils/LogUtils.kt
+++ EasyTools/src/main/java/org/easy/tools/utils/LogUtils.kt
@@ -9,25 +9,31 @@ import org.easy.tools.BuildConfig
  * @Description:
  */
 class LogUtils {
-    private val TAG = "LogUtils"
 
 
-    /**
-     * 打印普通日志
-     */
-    fun d(tag: String, str: String?) {
-        if (BuildConfig.DEBUG) {
-            Log.d(TAG + "_" + tag, str!!)
+    companion object {
+        private val TAG = "LogUtils"
+        /**
+         * 打印普通日志
+         */
+        @JvmStatic
+        fun d(tag: String, str: String?) {
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG + "_" + tag, str!!)
+            }
         }
+
+        /**
+         * 打印错误日志
+         */
+        @JvmStatic
+        fun e(tag: String, str: String?) {
+            if (BuildConfig.DEBUG) {
+                Log.e(TAG + "_" + tag, str!!)
+            }
+        }
+
     }
 
-    /**
-     * 打印错误日志
-     */
-    fun e(tag: String, str: String?) {
-        if (BuildConfig.DEBUG) {
-            Log.e(TAG + "_" + tag, str!!)
-        }
-    }
 
 }
